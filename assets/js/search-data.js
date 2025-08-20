30d38f725540bcea05937b1dd3c7d15ff6bd1282
chenabnavalkha <chenab.navalkha@gmail.com>
2025-08-20 20:37:29 +0000

    Deploying to gh-pages from @ chenabnavalkha/chenabnavalkha.github.io@3aed0dc44136efe6644db741921d808e5d7794d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -497,18 +497,25 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("chenab.bsky.social", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%63%68%65%6E%61%62@%6D%69%74.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,7 +529,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=jd02bBkAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
